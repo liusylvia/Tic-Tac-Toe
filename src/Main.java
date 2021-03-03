9f39f065040f296d2bb9730f5b85a6298b3ba6e7
Sylvia Liu <sylvs.liu@gmail.com>
2021-03-02 17:03:42 -0800

    Added tttBoard

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
 
     public static void main(String[] args) {
-        // hello
+
+        char[][] tttBoard = new char[][]{
+                {' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' '},
+                {'-', '-', '-', '-', '-', '+', '-', '-', '-', '-', '-', '+', '-', '-', '-', '-', '-'},
+                {' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' '},
+                {'-', '-', '-', '-', '-', '+', '-', '-', '-', '-', '-', '+', '-', '-', '-', '-', '-'},
+                {' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' '}};
+
+        createBoard(tttBoard);
+
     }
+
+    public static void createBoard(char[][] tttBoard) {
+        for (char[] row : tttBoard) {
+            for (char c : row)
+                System.out.print(c);
+            System.out.println();
+        }
+    }
+
+
 }
+
